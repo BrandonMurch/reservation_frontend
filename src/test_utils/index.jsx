fb0cb8cca2bb2b1dd9b5cd88e8e399bb5114304e
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-07-23 17:05:37 +0300

    Add renderWithRouter and mockFetch for tests

---
+++ src/test_utils/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import PropTypes from 'prop-types';
+
+// eslint-disable-next-line import/prefer-default-export
+export const renderWithRouter = function createWrapperForRouting(ui, {
+  route = '/',
+  history = createMemoryHistory({
+    initialEntries: [route],
+  }),
+}) {
+  const Wrapper = ({ children }) => (
+    <Router history={history}>
+      {children}
+    </Router>
+  );
+
+  Wrapper.propTypes = {
+    children: PropTypes.element,
+  };
+
+  Wrapper.defaultProps = {
+    children: <div />,
+  };
+
+  return {
+    ...render(ui, { wrapper: Wrapper }),
+    history,
+  };
+};
+
+export const mockFetch = function mockSuccessfulResponse(
+  status,
+  returnBody,
+) {
+  return new Promise((resolve) => {
+    resolve({
+      ok: true,
+      status: (status || 201),
+      json: () => Promise.resolve(returnBody || {}),
+    });
+  });
+};
