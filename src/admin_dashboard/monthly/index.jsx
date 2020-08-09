e8da2405288159c9febb9c6802aa02ddc97f9014
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-08-09 18:02:08 +0300

    Remove FullCalendar from monthly view
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/admin_dashboard/monthly/index.jsx
+++ src/admin_dashboard/monthly/index.jsx
@@ -1,15 +1,11 @@
 // Dependencies
-import React, { useState } from 'react';
+import React from 'react';
 // import PropTypes from 'prop-types';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
 
 // Stylesheet
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import Calendar from 'general_components/calendar';
 import style from './monthly.module.css';
-import Box from './calendar_box';
 
 const Monthly = () => (
   <div className={style.container}>
