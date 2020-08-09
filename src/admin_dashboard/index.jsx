e526b761050dde2dde361037c040e29817a13b0d
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-08-09 11:54:01 +0300

    Add temporary Monthly placeholder
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/admin_dashboard/index.jsx
+++ src/admin_dashboard/index.jsx
@@ -1,9 +1,11 @@
 // Dependencies
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 // Components
 import Nav from './nav_bar';
 import NavItem from './nav_bar/item';
+import Monthly from './monthly';
 
 const Dashboard = function AdminDashboard() {
   return (
@@ -13,6 +15,9 @@ const Dashboard = function AdminDashboard() {
         <NavItem label="Daily Run Sheet" to="/admin/daily" />
         <NavItem label="Options" to="/admin/options" />
       </Nav>
+      <Switch>
+        <Route exact path="/admin/monthly" component={Monthly} />
+      </Switch>
     </div>
   );
 };
