9c8b7e395594e61afc5cbcfc0771d2800ce1e552
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-08-09 11:54:01 +0300

    Add temporary Monthly placeholder
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

---
+++ src/admin_dashboard/monthly/index.jsx
@@ -0,0 +1,19 @@
+// Dependencies
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+// Stylesheet
+import { Redirect } from 'react-router-dom';
+import style from './monthly.module.css';
+import Box from './calendar_box';
+
+const Monthly = () =>
+// const [redirect, setRedirect] = useState('');
+
+  (
+    <div>Monthly placeholder</div>
+  );
+export default Monthly;
