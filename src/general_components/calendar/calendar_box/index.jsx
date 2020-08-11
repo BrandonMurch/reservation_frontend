8cd1c563b745afb43728f57d1a9649d2b5b3181c
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-08-11 06:32:43 +0300

    Add aesthetic changes to calendar
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/general_components/calendar/calendar_box/index.jsx
+++ src/general_components/calendar/calendar_box/index.jsx
@@ -1,11 +1,14 @@
 // Dependencies
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 // Stylesheets
 import style from './calendar_box.module.css';
 
-const Box = ({ date, message, dateObject }) => {
+const Box = ({
+  date, message, dateObject,
+}) => {
   const click = function onClick() { console.log(dateObject.set('date', date).format('YYYY-MM-DD')); };
 
   const text = (
@@ -15,17 +18,27 @@ const Box = ({ date, message, dateObject }) => {
     </>
   );
 
-  const containerStyle = date ? style.activeContainer : style.container;
+  const today = moment().startOf('date');
+  let containerStyle;
+  if (date === null) {
+    containerStyle = style.container;
+  } else if (dateObject.set('date', date).isSame(today)) {
+    containerStyle = style.todayContainer;
+  } else {
+    containerStyle = style.activeContainer;
+  }
 
   return (
     <td className={containerStyle}>
       {date !== null
-        ? (
-          <button className={style.button} type="button" onClick={click} onKeyUp={click}>
-            {text}
-          </button>
-        )
-        : text}
+        && (
+          <label className={style.hiddenLabelText} htmlFor="button">
+            {date}
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button className={style.button} type="button" name="button" onClick={click} onKeyUp={click} />
+          </label>
+        )}
+      {text}
 
     </td>
   );
