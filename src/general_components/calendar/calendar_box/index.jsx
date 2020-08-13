c372650da1b53216b68363572db34e62053544d1
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-08-13 05:13:57 +0300

    Change CalendarBox to only take a date string
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/general_components/calendar/calendar_box/index.jsx
+++ src/general_components/calendar/calendar_box/index.jsx
@@ -7,57 +7,48 @@ import moment from 'moment';
 import style from './calendar_box.module.css';
 
 const Box = ({
-  date, message, dateObject,
+  message, date,
 }) => {
-  const click = function onClick() { console.log(dateObject.set('date', date).format('YYYY-MM-DD')); };
+  const click = function onClick() {
+    if (date) {
+      // eslint-disable-next-line no-console
+      console.log(date);
+    }
+  };
 
-  const text = (
-    <>
-      <h1 className={style.dateText}>{date}</h1>
-      <h1 className={style.messageText}>{message}</h1>
-    </>
-  );
-
-  const today = moment().startOf('date');
+  const today = moment().startOf('date').format('YYYY-MM-DD');
   let containerStyle;
-  if (date === null) {
+  if (date === '') {
     containerStyle = style.container;
-  } else if (dateObject.set('date', date).isSame(today)) {
+  } else if (date === today) {
     containerStyle = style.todayContainer;
   } else {
     containerStyle = style.activeContainer;
   }
 
-  return (
-    <td className={containerStyle}>
-      {date !== null
-        && (
-          <label className={style.hiddenLabelText} htmlFor="button">
-            {date}
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button className={style.button} type="button" name="button" onClick={click} onKeyUp={click} />
-          </label>
-        )}
-      {text}
+  const dateNumber = date.substr(8).replace(/^0/, '');
 
+  return (
+    <td className={containerStyle} role="gridcell" onClick={click} onKeyUp={click}>
+      {date !== ''
+        && (
+          <>
+            <h1 className={style.dateText}>{dateNumber}</h1>
+            <h1 className={style.messageText}>{message}</h1>
+          </>
+        )}
     </td>
   );
 };
 
 Box.propTypes = {
-  date: PropTypes.number,
   message: PropTypes.string,
-  dateObject: PropTypes.shape({
-    set: PropTypes.func,
-  }),
+  date: PropTypes.string,
 };
 
 Box.defaultProps = {
-  date: null,
   message: null,
-  dateObject: {
-    date: () => '',
-  },
+  date: '',
 };
 
 export default Box;
