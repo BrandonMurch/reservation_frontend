87c858513b743f0ca7b22f307dd49e1c75d3f585
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-08-14 11:55:03 +0300

    Add tests for Header
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

---
+++ src/general_components/calendar/header/__test__/header.test.jsx
@@ -0,0 +1,55 @@
+// Dependencies
+import React from 'react';
+import { unmountComponentAtNode } from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { create } from 'react-test-renderer';
+
+// Components
+import Header from '../index';
+
+describe('<Header />', () => {
+  let component;
+  let container = null;
+  const props = {
+    month: 'August 2020',
+    prev: jest.fn(),
+    next: jest.fn(),
+    isThisMonth: false,
+    goToCurrentMonth: jest.fn(),
+
+  };
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    component = render(<Header {...props} />);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it('should match snapshot', () => {
+    const tree = create(<Header {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should display current month', () => {
+    const month = component.getByText('August 2020');
+    expect(month).toBeInTheDocument();
+  });
+
+  it('call functions with buttons', () => {
+    let button = component.getByRole('button', { name: 'Today' });
+    fireEvent.click(button);
+    expect(props.goToCurrentMonth).toHaveBeenCalled();
+    button = component.getByRole('button', { name: '>' });
+    fireEvent.click(button);
+    expect(props.next).toHaveBeenCalled();
+    button = component.getByRole('button', { name: '<' });
+    fireEvent.click(button);
+    expect(props.prev).toHaveBeenCalled();
+  });
+});
