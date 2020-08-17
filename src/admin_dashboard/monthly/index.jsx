a9d66f064e648de0ac9defa39acd15629c86e8dd
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-08-17 17:04:48 +0300

    Add monthly view for reservations
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/admin_dashboard/monthly/index.jsx
+++ src/admin_dashboard/monthly/index.jsx
@@ -1,19 +1,44 @@
 // Dependencies
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import fetchWrapper from 'shared/fetch';
 
-// Stylesheet
-// import { Redirect } from 'react-router-dom';
+// Components
 import Calendar from 'general_components/calendar';
+import Loading from 'general_components/loading';
+
+// Stylesheets
 import style from './monthly.module.css';
 
-const onDateRender = function calendarBoxOptions(/* { date, setMessage } */) {
-  // TODO
+const Monthly = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [reservationMap, setReservationMap] = useState({});
+  useEffect(() => {
+    fetchWrapper('/bookings/dailyCount', 'GET')
+      .then(
+        (response) => {
+          setIsLoaded(true);
+          setReservationMap(response);
+        },
+        (error) => {
+        // TODO: SetError banner
+          console.error(error);
+          setIsLoaded(true);
+        },
+      );
+  }, []);
+  if (!isLoaded) {
+    return <Loading />;
+  }
+  return (
+    <div className={style.container}>
+      <Calendar onDateRender={({ date, setMessage }) => {
+        if (reservationMap[date]) {
+          setMessage(`${reservationMap[date]} reservations`);
+        }
+      }}
+      />
+    </div>
+  );
 };
 
-const Monthly = () => (
-  <div className={style.container}>
-    <Calendar onDateRender={onDateRender} />
-  </div>
-);
 export default Monthly;
