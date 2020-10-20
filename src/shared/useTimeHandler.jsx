969c8cc01dc252d34269b43ae3a50f78ed34a9ec
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-20 10:48:17 +0300

    Add goTo and set to TimeHandler actions
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/shared/useTimeHandler.jsx
+++ src/shared/useTimeHandler.jsx
@@ -1,18 +1,22 @@
 import { useReducer } from 'react';
 import moment from 'moment';
 
-const useTimeHandler = (timeDuration, initialDate = moment().format()) => {
+const useTimeHandler = (initialDate = moment()) => {
   const initialDateObject = {
     dateObject: moment(initialDate),
   };
   const reducer = ((state, action) => {
-    switch (action) {
+    switch (action.type) {
       case 'prev':
-        return { dateObject: state.dateObject.subtract(1, timeDuration) };
+        return { dateObject: state.dateObject.subtract(1, action.unit) };
       case 'next':
-        return { dateObject: state.dateObject.add(1, timeDuration) };
+        return { dateObject: state.dateObject.add(1, action.unit) };
       case 'current':
         return { dateObject: moment() };
+      case 'goTo':
+        return { dateObject: moment(action.date) };
+      case 'set':
+        return { dateObject: state.dateObject.set(action.unit, action.number) };
       default: throw new Error('No such action exists.');
     }
   });
