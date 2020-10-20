41cb82c07bd0c7df25605709e74086b2cf603af4
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-20 10:52:04 +0300

    Add click function for datePicker
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/general_components/calendar/header/date_picker/index.jsx
+++ src/general_components/calendar/header/date_picker/index.jsx
@@ -11,34 +11,42 @@ import MonthYearSelector from './month_selector';
 // Stylesheets
 import style from './date_picker.module.css';
 
-const DatePicker = function SmallWindowToSelectDate({ dateObject, dispatchDate, dateClick }) {
+const DatePicker = function SmallWindowToSelectDate({ dateObject, dispatchDate }) {
   const [displayMonthSelector, setDisplayMonthSelector] = useState(false);
-  console.log(dateObject);
   return (
     <>
-      <button type="button" onClick={() => setDisplayMonthSelector(true)}>
+      <button type="button" onClick={() => setDisplayMonthSelector(!displayMonthSelector)}>
         {dateObject.format('MMMM YYYY')}
       </button>
       <div className={style.container}>
         {displayMonthSelector
           ? (
             <MonthYearSelector
+              className={style.datePicker}
               dateObject={dateObject}
               dispatchDate={dispatchDate}
-              className={style.datePicker}
             />
           )
           : (
             <CalendarBody
-              dateObject={dateObject}
-              onClick={dateClick}
               className={style.datePicker}
+              dateObject={dateObject}
+              onClick={(date) => {
+                dispatchDate({ type: 'goTo', date });
+              }}
             />
           )}
       </div>
 
     </>
   );
+};
+
+DatePicker.propTypes = {
+  dateObject: PropTypes.shape({
+    format: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatchDate: PropTypes.func.isRequired,
 };
 
 export default DatePicker;
