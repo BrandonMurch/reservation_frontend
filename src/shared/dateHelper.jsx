e6ded6a14de5bbaab1045c9e6eea00d9f326445d
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-20 10:48:48 +0300

    Fix short property making it lowercase
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/shared/dateHelper.jsx
+++ src/shared/dateHelper.jsx
@@ -1,40 +1,44 @@
 /* eslint-disable import/prefer-default-export */
 
 export const getMonth = function getMonthFromNumber(monthNumber) {
-  const months = [{
-    long: 'January',
-    Short: 'Jan',
-  }, {
-    long: 'February',
-    Short: 'Feb',
-  }, {
-    long: 'March',
-    Short: 'Mar',
-  }, {
-    long: 'April',
-    Short: 'Apr',
-  }, {
-    long: 'May',
-    Short: 'May',
-  }, {
-    long: 'June',
-    Short: 'Jun',
-  }, {
-    long: 'August',
-    Short: 'Aug',
-  }, {
-    long: 'September',
-    Short: 'Sep',
-  }, {
-    long: 'October',
-    Short: 'Oct',
-  }, {
-    long: 'November',
-    Short: 'Nov',
-  }, {
-    long: 'December',
-    Short: 'Dec',
-  },
+  const months = [
+    {
+      long: 'January',
+      short: 'Jan',
+    }, {
+      long: 'February',
+      short: 'Feb',
+    }, {
+      long: 'March',
+      short: 'Mar',
+    }, {
+      long: 'April',
+      short: 'Apr',
+    }, {
+      long: 'May',
+      short: 'May',
+    }, {
+      long: 'June',
+      short: 'Jun',
+    }, {
+      long: 'July',
+      short: 'Jul',
+    }, {
+      long: 'August',
+      short: 'Aug',
+    }, {
+      long: 'September',
+      short: 'Sep',
+    }, {
+      long: 'October',
+      short: 'Oct',
+    }, {
+      long: 'November',
+      short: 'Nov',
+    }, {
+      long: 'December',
+      short: 'Dec',
+    },
 
   ];
 
