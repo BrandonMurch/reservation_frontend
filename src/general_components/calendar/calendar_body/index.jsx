c6224ecf647315a908473017e39d73f5e13c2cdd
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-20 11:41:15 +0300

    Allow day titles in calendar to be editable
    Add proptypes to calendar body
    
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/general_components/calendar/calendar_body/index.jsx
+++ src/general_components/calendar/calendar_body/index.jsx
@@ -9,9 +9,9 @@ import CalendarRows from './calendar_rows';
 // Stylesheets
 import style from './calendar_body.module.css';
 
-const ColumnHeaders = function GetTitlesForDaysOfWeek() {
+const ColumnHeaders = function GetTitlesForDaysOfWeek({ titleStyle }) {
   const dayTitles = moment.weekdaysShort().map((day) => (
-    <th key={day} className={style.column}>
+    <th key={day} className={titleStyle || style.column}>
       {day}
     </th>
   ));
@@ -23,15 +23,38 @@ const ColumnHeaders = function GetTitlesForDaysOfWeek() {
   );
 };
 
-const CalendarBody = function CalendarWithDayCells({ dateObject, onDateRender, onClick }) {
+ColumnHeaders.propTypes = {
+  titleStyle: PropTypes.shape({}),
+};
+
+ColumnHeaders.defaultProps = {
+  titleStyle: null,
+};
+
+const CalendarBody = function CalendarWithDayCells({
+  dateObject, onDateRender, onClick, titleStyle,
+}) {
   return (
     <table className={style.table} role="grid">
       <thead>
-        <ColumnHeaders />
+        <ColumnHeaders titleStyle={titleStyle} />
       </thead>
       <CalendarRows dateObject={dateObject} onDateRender={onDateRender} onClick={onClick} />
     </table>
   );
+};
+
+CalendarBody.propTypes = {
+  dateObject: PropTypes.shape({}).isRequired,
+  onDateRender: PropTypes.func,
+  onClick: PropTypes.func,
+  titleStyle: PropTypes.shape({}),
+};
+
+CalendarBody.defaultProps = {
+  onDateRender: () => {},
+  onClick: () => {},
+  titleStyle: {},
 };
 
 export default CalendarBody;
