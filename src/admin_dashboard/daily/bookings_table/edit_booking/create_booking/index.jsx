941ebdfe7351e019253203b2c31f26d5330d7e3f
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-23 02:18:29 +0300

    Add createBooking window to /admin/dailySet default view of window to CreateAllow choosing which window to selectRefactor functions outside of function
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

---
+++ src/admin_dashboard/daily/bookings_table/edit_booking/create_booking/index.jsx
@@ -0,0 +1,107 @@
+// Dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+import Form from 'general_components/form';
+
+// Stylesheets
+import style from '../edit_window/edit_window.module.css';
+
+const getInputs = function getInputsFromBooking(date) {
+  // TODO: remove default values that were used for testing
+  return [{
+    name: 'date',
+    type: 'date',
+    label: 'Date',
+    value: date || undefined,
+    required: true,
+  },
+  {
+    name: 'startTime',
+    type: 'time',
+    label: 'Start Time',
+    value: '20:00',
+    required: true,
+  },
+  {
+    name: 'endTime',
+    type: 'time',
+    label: 'End Time',
+    value: '23:00',
+    required: true,
+  },
+  {
+    name: 'partySize',
+    type: 'text',
+    label: 'Party Size',
+    required: true,
+    value: '2',
+    pattern: '^[1-9][0-9]*$',
+    patternMessage: 'Party size must be a number greater than zero',
+  },
+  {
+    name: 'name',
+    type: 'text',
+    label: 'Name',
+    value: 'John Johnson',
+    required: true,
+  },
+  {
+    name: 'email',
+    type: 'email',
+    label: 'Email',
+    required: true,
+    value: 'John@john.com',
+    pattern: '^[\\w\\-_.+]*[\\w\\-_.]@([\\w]+\\.)+[\\w]+[\\w]$',
+    patternMessage: 'Email must be properly formatted',
+  },
+  {
+    name: 'phoneNumber',
+    type: 'tel',
+    label: 'Phone Number',
+    required: true,
+    value: '+1 12323232',
+    pattern: '^\\+\\d{1,3} \\d{6,14}$',
+    patternMessage:
+        'Phone number must be in the format of +1 123456789 where +1 is the country code, followed by the phone number',
+  },
+  {
+    name: 'restaurantComments',
+    type: 'text-area',
+    label: 'Restaurant Comments',
+    value: 'comment',
+    required: false,
+  },
+  {
+    name: 'userComments',
+    type: 'text-area',
+    label: 'User Comments',
+    required: false,
+  },
+  ];
+};
+
+const CreateBooking = ({
+  date, onSubmit,
+}) => (
+  <div className={style.formContainer}>
+    <Form
+      inputs={getInputs(date)}
+      onSubmit={(event) => {
+        event.startTime = `${event.date}T${event.startTime}`;
+        event.endTime = `${event.date}T${event.endTime}`;
+        event.partySize = Number.parseInt(event.partySize, 10);
+        onSubmit(event);
+      }}
+      submitLabel="Create booking"
+      styleProp={style}
+    />
+  </div>
+
+);
+
+CreateBooking.propTypes = {
+  date: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default CreateBooking;
