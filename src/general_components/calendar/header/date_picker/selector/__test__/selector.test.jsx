913156f0ad521d204c56cd0d9985754eb0871124
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-22 10:40:23 +0300

    Add test for selector
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/general_components/calendar/header/date_picker/selector/__test__/selector.test.jsx
+++ src/general_components/calendar/header/date_picker/selector/__test__/selector.test.jsx
@@ -4,6 +4,7 @@ import { unmountComponentAtNode } from 'react-dom';
 import { render, fireEvent } from '@testing-library/react';
 import { create } from 'react-test-renderer';
 import moment from 'moment';
+import { getMonth } from 'shared/dateHelper';
 
 // Components
 import Selector from '../index';
@@ -12,7 +13,6 @@ describe('<Selector />', () => {
   let component;
   let container = null;
   const props = {
-    setDisplayDatePicker: jest.fn(),
     dateObject: moment(),
     dispatchDate: jest.fn(),
   };
@@ -24,6 +24,7 @@ describe('<Selector />', () => {
   });
 
   afterEach(() => {
+    props.dispatchDate.mockClear();
     unmountComponentAtNode(container);
     container.remove();
     container = null;
@@ -34,15 +35,65 @@ describe('<Selector />', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it('should display current month', () => {
-    const month = component.getByText(props.dateObject.format('MMMM YYYY'));
-    expect(month).toBeInTheDocument();
+  it('should change the year when a year button is pressed', () => {
+    const year = props.dateObject.year() + 1;
+    const button = component.getByRole('button', { name: year.toString() });
+    fireEvent.click(button);
+    expect(props.dispatchDate).toHaveBeenCalledWith({
+      unit: 'years',
+      number: year,
+      type: 'set',
+    });
   });
 
-  it('should switch view when button is pressed', () => {
-    const button = component.getByText(props.dateObject.format('MMMM YYYY'));
+  it('should augment the year when the up button is pressed', () => {
+    const button = component.getByLabelText('years up');
     fireEvent.click(button);
-    const up = component.getByLabelText('months up');
-    expect(up).toBeInTheDocument();
+    expect(props.dispatchDate).toHaveBeenCalledWith({
+      unit: 'years',
+      number: -1,
+      type: 'jumpUnit',
+    });
+  });
+
+  it('should decrement the year when the down button is pressed', () => {
+    const button = component.getByLabelText('years down');
+    fireEvent.click(button);
+    expect(props.dispatchDate).toHaveBeenCalledWith({
+      unit: 'years',
+      number: 1,
+      type: 'jumpUnit',
+    });
+  });
+  it('should change the month when a month button is pressed', () => {
+    const month = props.dateObject.month() + 1;
+    const monthText = getMonth(month).short;
+    const button = component.getByRole('button', { name: monthText });
+    fireEvent.click(button);
+    expect(props.dispatchDate).toHaveBeenCalledWith({
+      unit: 'months',
+      number: month,
+      type: 'set',
+    });
+  });
+
+  it('should augment the month when the up button is pressed', () => {
+    const button = component.getByLabelText('months up');
+    fireEvent.click(button);
+    expect(props.dispatchDate).toHaveBeenCalledWith({
+      unit: 'months',
+      number: -1,
+      type: 'jumpUnit',
+    });
+  });
+
+  it('should decrement the month when the down button is pressed', () => {
+    const button = component.getByLabelText('months down');
+    fireEvent.click(button);
+    expect(props.dispatchDate).toHaveBeenCalledWith({
+      unit: 'months',
+      number: 1,
+      type: 'jumpUnit',
+    });
   });
 });
