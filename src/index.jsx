2b6e9a49afd1a693058e1d5be1e996fe3b7e9023
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-22 06:36:06 +0300

    Disable StrictMode for better testing
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/index.jsx
+++ src/index.jsx
@@ -10,10 +10,10 @@ import App from './app';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Router>
+    <App />
+  </Router>,
+  // </React.StrictMode>,
   document.getElementById('root'),
 );
