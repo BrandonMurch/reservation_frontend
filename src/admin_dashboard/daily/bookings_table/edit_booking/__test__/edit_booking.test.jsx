a72787d41412b2446a15865d9bfe309495717fe8
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-25 10:21:09 +0200

    Refactor tests
    Use screen instead of component
    Cleanup between tests
    
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/admin_dashboard/daily/bookings_table/edit_booking/__test__/edit_booking.test.jsx
+++ src/admin_dashboard/daily/bookings_table/edit_booking/__test__/edit_booking.test.jsx
@@ -1,7 +1,7 @@
 // Dependencies
 import React from 'react';
 import {
-  render, fireEvent, screen, cleanup,
+  render, fireEvent, screen, cleanup, act,
 } from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import { create } from 'react-test-renderer';
@@ -12,7 +12,6 @@ import types from '../window_types';
 import EditBooking from '../index';
 
 describe('<EditBooking />', () => {
-  let component;
   let container = null;
   const props = {
     exit: jest.fn(),
@@ -29,28 +28,23 @@ describe('<EditBooking />', () => {
     },
   };
 
-  const clearRender = () => {
-    jest.clearAllMocks();
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-  };
-
   const buildComponent = () => {
     container = document.createElement('div');
     document.body.appendChild(container);
-    component = render(
+    render(
       <EditBooking {...props} />,
     );
   };
 
   beforeEach(async () => {
-    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ status: 200 }));
     buildComponent();
   });
 
   afterEach(() => {
-    clearRender();
+    jest.clearAllMocks();
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
   });
 
   it('should match snapshot', () => {
@@ -61,49 +55,60 @@ describe('<EditBooking />', () => {
   });
 
   it('should display an exit button', () => {
-    const exitButton = component.getByRole('button', { name: 'X' });
+    const exitButton = screen.getByRole('button', { name: 'X' });
     expect(exitButton).toBeInTheDocument();
   });
 
   it('should display create window', () => {
-    const createDisplay = component.getByText(/Create (b|B)ooking/i);
+    const createDisplay = screen.getByText(/Create (b|B)ooking/i);
     expect(createDisplay).toBeInTheDocument();
   });
 
   it('should display edit window', async () => {
     props.entryWindow = types.EDIT;
     buildComponent();
-    const editDisplay = component.getByText(/Save (b|B)ooking/i);
+    const editDisplay = screen.getByText(/Save booking/i);
     expect(editDisplay).toBeInTheDocument();
   });
 
   it('should display delete window', async () => {
     props.entryWindow = types.DELETE;
     buildComponent();
-    const editDisplay = component.getByText(/delete the booking/i);
+    const editDisplay = screen.getByText(/delete the booking/i);
     expect(editDisplay).toBeInTheDocument();
     const submitButton = screen.getByRole('button', { name: 'Yes' });
     fireEvent.click(submitButton);
   });
 
-  it('should call setErrorBanner on error', async () => {
-    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.reject(new Error('Error!')));
+  it('should call exit after fetch', async () => {
+    cleanup();
+    const jestSpy = jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ status: 200 }));
+    buildComponent();
+    const submitButton = screen.getByRole('button', { name: 'Yes' });
+    await act(async () => {
+      await fireEvent.click(submitButton);
+    });
+    expect(jestSpy).toBeCalledTimes(1);
+    expect(props.exit).toBeCalledTimes(1);
+  });
+
+  it('should display loading after fetch', () => {
+    cleanup();
+    const jestSpy = jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ status: 200 }));
+    buildComponent();
+    const submitButton = screen.getByRole('button', { name: 'Yes' });
+    fireEvent.click(submitButton);
+    expect(jestSpy).toHaveBeenCalledTimes(1);
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+  });
+
+  it('should call setErrorBanner on error', () => {
+    const jestSpy = jest.spyOn(global, 'fetch').mockImplementation(() => Promise.reject(new Error('Error!')));
     cleanup();
     buildComponent();
     const submitButton = screen.getByRole('button', { name: 'Yes' });
     fireEvent.click(submitButton);
-    expect(props.setErrorBanner).toHaveBeenCalled();
-  });
-
-  it('should call exit after fetch', () => {
-    const submitButton = screen.getByRole('button', { name: 'Yes' });
-    fireEvent.click(submitButton);
-    expect(props.exit).toHaveBeenCalled();
-  });
-
-  it('should display loading after fetch', async () => {
-    const submitButton = screen.getByRole('button', { name: 'Yes' });
-    fireEvent.click(submitButton);
-    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    expect(jestSpy).toHaveBeenCalledTimes(1);
+    expect(props.setErrorBanner).toHaveBeenCalledTimes(1);
   });
 });
