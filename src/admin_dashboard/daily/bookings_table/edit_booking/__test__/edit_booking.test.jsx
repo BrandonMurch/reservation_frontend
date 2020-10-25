35ded622ce1a90ea937bdd2eb9bfc81d065ce8fa
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-25 12:05:36 +0200

    Refactor EditBooking Test
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/admin_dashboard/daily/bookings_table/edit_booking/__test__/edit_booking.test.jsx
+++ src/admin_dashboard/daily/bookings_table/edit_booking/__test__/edit_booking.test.jsx
@@ -1,50 +1,45 @@
 // Dependencies
 import React from 'react';
 import {
-  render, fireEvent, screen, cleanup, act,
+  render, fireEvent, screen, act,
 } from '@testing-library/react';
-import { unmountComponentAtNode } from 'react-dom';
 import { create } from 'react-test-renderer';
-import moment from 'moment';
 import types from '../window_types';
 
 // Components
 import EditBooking from '../index';
 
 describe('<EditBooking />', () => {
-  let container = null;
-  const props = {
-    exit: jest.fn(),
-    setErrorBanner: jest.fn(),
-    entryWindow: types.CREATE,
-    date: '2020-10-24',
-    booking: {
-      id: 20,
-      startTime: moment().format(),
-      partySize: 2,
-      user: {
-        firstName: 'name',
-      },
-    },
-  };
+  let props;
 
-  const buildComponent = () => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-    render(
-      <EditBooking {...props} />,
-    );
-  };
+  const buildComponent = () => render(
+    <EditBooking {...props} />,
+  );
 
   beforeEach(async () => {
+    jest
+      .spyOn(global.console, 'error')
+      .mockImplementation(() => {});
+
+    props = {
+      exit: jest.fn(),
+      setErrorBanner: jest.fn(),
+      entryWindow: types.DELETE,
+      date: '2020-10-24',
+      booking: {
+        id: 20,
+        startTime: '2020-10-24T20:00',
+        partySize: 2,
+        user: {
+          firstName: 'name',
+        },
+      },
+    };
     buildComponent();
   });
 
   afterEach(() => {
     jest.clearAllMocks();
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
   });
 
   it('should match snapshot', () => {
@@ -60,6 +55,8 @@ describe('<EditBooking />', () => {
   });
 
   it('should display create window', () => {
+    props.entryWindow = types.CREATE;
+    buildComponent();
     const createDisplay = screen.getByText(/Create (b|B)ooking/i);
     expect(createDisplay).toBeInTheDocument();
   });
@@ -72,18 +69,12 @@ describe('<EditBooking />', () => {
   });
 
   it('should display delete window', async () => {
-    props.entryWindow = types.DELETE;
-    buildComponent();
     const editDisplay = screen.getByText(/delete the booking/i);
     expect(editDisplay).toBeInTheDocument();
-    const submitButton = screen.getByRole('button', { name: 'Yes' });
-    fireEvent.click(submitButton);
   });
 
   it('should call exit after fetch', async () => {
-    cleanup();
     const jestSpy = jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ status: 200 }));
-    buildComponent();
     const submitButton = screen.getByRole('button', { name: 'Yes' });
     await act(async () => {
       await fireEvent.click(submitButton);
@@ -93,21 +84,19 @@ describe('<EditBooking />', () => {
   });
 
   it('should display loading after fetch', () => {
-    cleanup();
     const jestSpy = jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ status: 200 }));
-    buildComponent();
     const submitButton = screen.getByRole('button', { name: 'Yes' });
     fireEvent.click(submitButton);
     expect(jestSpy).toHaveBeenCalledTimes(1);
     expect(screen.getByText(/loading/i)).toBeInTheDocument();
   });
 
-  it('should call setErrorBanner on error', () => {
+  it('should call setErrorBanner on error', async () => {
     const jestSpy = jest.spyOn(global, 'fetch').mockImplementation(() => Promise.reject(new Error('Error!')));
-    cleanup();
-    buildComponent();
     const submitButton = screen.getByRole('button', { name: 'Yes' });
-    fireEvent.click(submitButton);
+    await act(async () => {
+      await fireEvent.click(submitButton);
+    });
     expect(jestSpy).toHaveBeenCalledTimes(1);
     expect(props.setErrorBanner).toHaveBeenCalledTimes(1);
   });
