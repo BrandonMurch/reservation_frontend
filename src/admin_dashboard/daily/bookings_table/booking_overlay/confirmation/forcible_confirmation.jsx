3b03df5b73227eb8d970a44beaa779af8908be65
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-10-26 11:25:45 +0200

    Add forcible option for updating tables
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

---
+++ src/admin_dashboard/daily/bookings_table/booking_overlay/confirmation/forcible_confirmation.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Confirmation from '.';
+
+const ForcibleConfirmation = function AskUserIfTheyWouldLikeToForcePreviousFetch({
+  error, previousFetch, exit,
+}) {
+  // TODO: how do you modify previousFetch headers from here...?
+
+  const props = {
+    message: `The following error occured: \n ${error} \n Would you like to force this action to occur?`,
+    confirm: () => previousFetch(),
+    cancel: () => exit(),
+  };
+
+  return (<Confirmation {...props} />);
+};
+
+ForcibleConfirmation.propTypes = {
+  error: PropTypes.string.isRequired,
+  previousFetch: PropTypes.func.isRequired,
+  exit: PropTypes.func.isRequired,
+};
+
+export default ForcibleConfirmation;
--- src/admin_dashboard/daily/bookings_table/booking_overlay/window_types.jsx
+++ src/admin_dashboard/daily/bookings_table/booking_overlay/window_types.jsx
@@ -1,5 +1,5 @@
 import { enumeration } from 'shared/helpers';
 
-const types = enumeration.singleValue('EDIT', 'DELETE', 'LOADING', 'CREATE');
+const types = enumeration.singleValue('EDIT', 'DELETE', 'LOADING', 'CREATE', 'FORCIBLE');
 
 export default types;
