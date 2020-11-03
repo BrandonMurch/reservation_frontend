92daef819f4704aedcd11f7f80b69130bdc008a4
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-11-03 09:06:03 +0200

    Group props in input to make code more readable
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/general_components/form/inputs/base_input/index.jsx
+++ src/general_components/form/inputs/base_input/index.jsx
@@ -2,26 +2,21 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Input = function CreateInputAndLabel(props) {
-  const {
-    key,
-    value: initialValue,
-    type,
-    name,
-    label,
-    required,
-    pattern,
-    updateValue,
-    patternMessage,
-    displayErrors,
-    onChange,
-    onFocus,
-    onBlur,
-    style,
-    hiddenLabel,
-    onMouseEnter,
-    onMouseLeave,
-  } = props;
+const Input = function CreateInputAndLabel({
+  key,
+  value: initialValue,
+  name,
+  label,
+  updateValue,
+  patternMessage,
+  displayErrors,
+  onChange,
+  onBlur,
+  style,
+  hiddenLabel,
+  onKeyDown,
+  ...props
+}) {
   const [value, setValue] = useState(initialValue);
   const [errorMessage, setErrorMessage] = useState('');
 
@@ -43,23 +38,20 @@ const Input = function CreateInputAndLabel(props) {
         {label}
       </label>
       <input
-        onFocus={onFocus}
-        required={required}
-        pattern={pattern}
+        // eslint-disable-next-line
+        {...props}
         placeholder={label}
         className={displayErrors ? style.displayError : style.input}
         value={value}
         id={name}
-        type={type}
         name={name}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
         onKeyDown={((event) => {
           // Fixes where enter is clicked on last input, and the field isn't updated.
           if (event.keyCode === 13) {
             event.preventDefault();
             event.target.blur();
           }
+          onKeyDown(event);
         })}
         onBlur={({ target }) => {
           setErrorMessage(target.validationMessage);
@@ -79,21 +71,16 @@ const Input = function CreateInputAndLabel(props) {
 
 Input.propTypes = {
   key: PropTypes.string,
-  type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   value: PropTypes.string,
   updateValue: PropTypes.func,
-  required: PropTypes.bool,
-  pattern: PropTypes.string,
   label: PropTypes.string.isRequired,
   displayErrors: PropTypes.bool,
-  onMouseEnter: PropTypes.func,
-  onMouseLeave: PropTypes.func,
   patternMessage: PropTypes.string,
   hiddenLabel: PropTypes.bool,
-  onFocus: PropTypes.func,
   onChange: PropTypes.func,
   onBlur: PropTypes.func,
+  onKeyDown: PropTypes.func,
   style: PropTypes.shape({
     hiddenLabelText: PropTypes.string.isRequired,
     labelText: PropTypes.string.isRequired,
@@ -107,17 +94,13 @@ Input.propTypes = {
 Input.defaultProps = {
   key: '',
   value: '',
-  required: false,
-  pattern: null,
   patternMessage: null,
   displayErrors: false,
   hiddenLabel: false,
   onChange: () => {},
   onBlur: () => {},
-  onFocus: () => {},
   updateValue: () => {},
-  onMouseEnter: () => {},
-  onMouseLeave: () => {},
+  onKeyDown: () => {},
 };
 
 export default Input;
