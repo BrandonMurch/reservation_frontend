7a59ec5245741355cbb95165e54021f76a779569
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-11-04 07:02:34 +0200

    Add loading spinner
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/general_components/loading/index.jsx
+++ src/general_components/loading/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// TODO: Make me!
-const Loading = function CreateLoadingScreen() {
-  return <h1>loading...</h1>;
+import style from './loading.module.css';
+
+const getSpinnerSize = (size) => ({
+  small: (<div className={style.smallSpinner} />),
+  normal: (<div className={style.spinner} />),
+  large: (<div className={style.largeSpinner} />),
+})[size];
+
+const Loading = function CreateLoadingScreen({ size }) {
+  return getSpinnerSize(size);
+};
+
+Loading.propTypes = {
+  size: PropTypes.string,
+};
+
+Loading.defaultProps = {
+  size: 'normal',
 };
 
 export default Loading;
