af754a1950fdcd021bdabedf8b59d72f306e1bdc
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-11-11 01:53:18 +0200

    Add secondary attribute to NavBar
    secondary nav bar will be directly below main bar
    
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/admin_dashboard/nav_bar/index.jsx
+++ src/admin_dashboard/nav_bar/index.jsx
@@ -5,12 +5,21 @@ import PropTypes from 'prop-types';
 // Stylesheets
 import style from './nav_bar.module.css';
 
-const Nav = function NavigationBar({ children }) {
-  return <div className={style.container}>{children}</div>;
+const Nav = function NavigationBar({ children, secondary }) {
+  return (
+    <div className={secondary ? style.secondary : style.container}>
+      {children}
+    </div>
+  );
 };
 
 Nav.propTypes = {
   children: PropTypes.node.isRequired,
+  secondary: PropTypes.bool,
+};
+
+Nav.defaultProps = {
+  secondary: false,
 };
 
 export default Nav;
