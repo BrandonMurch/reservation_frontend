a8919f183b4f611572435cca4cdf1b02d49f3a15
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-11-23 03:01:38 +0200

    Fix new lines in confirmation windows and banners
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/admin_dashboard/daily/bookings_table/booking_overlay/confirmation/index.jsx
+++ src/admin_dashboard/daily/bookings_table/booking_overlay/confirmation/index.jsx
@@ -7,36 +7,31 @@ import style from './confirmation.module.css';
 
 const Confirmation = ({
   message, cancel, confirm,
-}) => {
-  const messageWithNewLines = message
-    .split('\n')
-  // eslint-disable-next-line react/no-array-index-key
-    .map((item, i) => <p className={style.text} key={i}>{item}</p>);
+}) => (
+  <div className={style.container}>
 
-  return (
-    <div className={style.container}>
-
-      <div className={style.textContainer}>{messageWithNewLines}</div>
-
-      <button
-        className={style.button}
-        type="button"
-        onClick={() => confirm()}
-      >
-        Yes
-      </button>
-
-      <button
-        className={style.button}
-        type="button"
-        onClick={() => cancel()}
-      >
-        No
-
-      </button>
+    <div className={style.textContainer}>
+      <p className={style.text}>{message}</p>
     </div>
-  );
-};
+
+    <button
+      className={style.button}
+      type="button"
+      onClick={() => confirm()}
+    >
+      Yes
+    </button>
+
+    <button
+      className={style.button}
+      type="button"
+      onClick={() => cancel()}
+    >
+      No
+
+    </button>
+  </div>
+);
 
 Confirmation.propTypes = {
   message: PropTypes.string.isRequired,
