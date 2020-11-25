a61b7300feb654a65a84df68da98bacd273feb6a
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-11-26 01:38:27 +0200

    Add example bookings and tables in test utils
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/admin_dashboard/daily/__test__/daily.test.jsx
+++ src/admin_dashboard/daily/__test__/daily.test.jsx
@@ -4,65 +4,12 @@ import {
   screen, waitForElementToBeRemoved,
 } from '@testing-library/react';
 import { mockFetch, renderWithRouter } from 'test_utils';
+import { exampleBooking1, exampleBooking2, tables } from 'test_utils/examples';
 
 // Components
 import Daily from '../index';
 
-const bookings = [{
-  id: 3,
-  partySize: 1,
-  userComments: null,
-  restaurantComments: null,
-  date: '2020-11-24',
-  startTime: '2020-11-24T20:08:00',
-  endTime: '2020-11-24T22:08:00',
-  user: {
-    id: 2,
-    username: 'john@john.com',
-    permissions: [],
-    firstName: 'John',
-    lastName: 'Johnson',
-    phoneNumber: '+12 121212',
-    termsAndConditions: false,
-    comments: null,
-  },
-  tables: [
-    {
-      name: '2',
-      seats: 2,
-      priority: 1,
-    },
-  ],
-}, {
-  id: 6,
-  partySize: 2,
-  userComments: null,
-  restaurantComments: null,
-  date: '2020-11-24',
-  startTime: '2020-11-24T20:09:00',
-  endTime: '2020-11-24T22:09:00',
-  user: {
-    id: 5,
-    username: 'j@j.com',
-    permissions: [],
-    firstName: 'John',
-    lastName: '',
-    phoneNumber: '+12 121212',
-    termsAndConditions: false,
-    comments: null,
-  },
-  tables: [
-    {
-      name: '22',
-      seats: 2,
-      priority: 0,
-    },
-  ],
-}];
-
-const tables = [{ name: '22', seats: 2, priority: 0 },
-  { name: '2', seats: 2, priority: 1 },
-  { name: 'a', seats: 2, priority: 2 }];
+const bookings = [exampleBooking1, exampleBooking2];
 
 describe('<Daily />', () => {
   const props = {
