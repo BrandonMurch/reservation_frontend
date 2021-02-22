08bf3671642f940092f3b967b4d2f81e43bf96a8
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2021-02-22 14:34:07 +0200

    Replace AutoComplete with RadioOptions
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

---
+++ src/general_components/form/radio.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const RadioOptions = ({
+  listOfOptions, name, onChange, value,
+}) => (
+  <div>
+    {listOfOptions.map((option) => (
+      <>
+        <input type="radio" id={option} name={name} value={option} onClick={() => onChange(option)} checked={option.toLowerCase() === value.toLowerCase()} />
+        <label htmlFor={option}>{option}</label>
+      </>
+    ))}
+  </div>
+);
+
+RadioOptions.propTypes = {
+  listOfOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+export default RadioOptions;
