7b9972bb1fe148f97abd15ced4341356813c61a8
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2021-02-23 02:56:13 +0200

    Add Array methods
    Add equals
    
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

---
+++ src/shared/array_methods.js
@@ -0,0 +1,19 @@
+const equals = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const Arrays = {
+  equals,
+};
+
+export default Arrays;
