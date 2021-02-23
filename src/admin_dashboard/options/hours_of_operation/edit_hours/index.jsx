2fbaa5cf4978d820469be4cca4e3742669aff133
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2021-02-23 02:59:32 +0200

    Extract BookingTimes into it's own file
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/admin_dashboard/options/hours_of_operation/edit_hours/index.jsx
+++ src/admin_dashboard/options/hours_of_operation/edit_hours/index.jsx
@@ -1,16 +1,13 @@
 // Dependencies
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { enumeration } from 'shared/helpers';
 
 // Components
-import NumberInput from 'general_components/form/inputs/number';
-import RadioOptions from 'general_components/form/radio';
 import NewRow from './new_row';
 
 // Stylesheet
 import style from './edit_hours.module.css';
-import SpecificTime from './specific_time';
+import BookingTimes from './booking_times';
 
 const HoursRow = ({ open, close, remove }) => (
   <tr>
@@ -24,52 +21,6 @@ HoursRow.propTypes = {
   open: PropTypes.string.isRequired,
   close: PropTypes.string.isRequired,
   remove: PropTypes.func.isRequired,
-};
-
-const BookingTimes = ({ hours }) => {
-  const bookingTimeModes = enumeration.singleValue('INTERVAL', 'SPECIFIC');
-  const [bookingTimeMode, setBookingTimeMode] = useState(bookingTimeModes.INTERVAL);
-
-  const getBookingTimeModeOptions = () => {
-    const options = Object.keys(bookingTimeModes);
-    return options.map((option) => option[0] + option.slice(1, option.length).toLowerCase());
-  };
-
-  return (
-    <>
-      <RadioOptions
-        listOfOptions={getBookingTimeModeOptions()}
-        name="BookingTimeMode"
-        value={bookingTimeMode.value}
-        onChange={(value) => {
-          setBookingTimeMode(bookingTimeModes[value.toUpperCase()]);
-        }}
-      />
-      {(bookingTimeMode.value === bookingTimeModes.INTERVAL.value
-        && (
-        <>
-          <div className={style.intervalContainer}>
-            <NumberInput style={style} name="interval" hiddenLabel label="Interval in minutes" min="0" hideErrors />
-          </div>
-          <p className={style.intervalText}>minutes</p>
-        </>
-        ))}
-      {(bookingTimeMode.value === bookingTimeModes.SPECIFIC.value
-        && (
-        <SpecificTime
-          hours={hours}
-          style={style}
-          name="bookingTimes"
-          label="Booking times in 24h, seperated by commas"
-          pattern="^(\d{2}:\d{2}((, ?)|$))+"
-        />
-        ))}
-    </>
-  );
-};
-
-BookingTimes.propTypes = {
-  hours: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 const EditHours = ({
